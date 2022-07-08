3f7ab56560bd43935ab4fa49a3d9c1f7eaaeb1d7
luis-castaneda7 <luis_casta7@outlook.com>
2022-07-08 14:10:14 -0500

    added rules for code

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	/* class          instance    title         tags mask     isfloating   monitor 	iscornered*/
 	{ "feh",             NULL,       NULL,         ~0,           1,           -1,		1 },
 	{ "Opera",           NULL,       NULL,          1,           0,           -1,		0 },
-	{ "code",            NULL,       NULL,       1<<2,           0,           -1,		0 },
+	{ "Code",            NULL,       NULL,       1<<1,           0,           -1,		0 },
 	{ "discord",         NULL,       NULL,       1<<2,           0,           -1,		0 },
 	{ "obsidian",        NULL,       NULL,       1<<3,           0,           -1,		0 },
 };
